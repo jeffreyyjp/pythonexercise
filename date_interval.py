5ed22347bc779621652c5f9beb733340a07418f4
messi-yang <yangjianpeng@Hisense.ad>
2017-11-30 12:46:29 +0800

    add error check

--- date_interval.py
+++ date_interval.py
@@ -1,27 +1,42 @@
+# encoding: UTF-8
+
 """date_interval
 
 This program is used to get the duration between two dates. User should input two dates, and the program will output the duration.
 """
 
-from datetime import datetime
+import re, os
+from datetime import date
 
-print('The date format is MM/DD/YY, for example: 11/29/2017')
+def check_date_format(datestr):
+    """ check the date format input. """
+    while re.search('\d\d/\d\d/\d\d\d\d', datestr) == None:
+        datestr = raw_input('Invalid date format, try again: ')
+    return datestr
 
-first_date = raw_input('Please input first date: ') # get user's first date input
-second_date = raw_input('Please input second date: ') # get user's second date input
-
-def check_date(datestr):
+def get_date(datestr, dateflag):
+    """ Get date object. """
     date_list = datestr.split('/')
     monthstr, daystr, yearstr = date_list[0], date_list[1], date_list[2]
+    print("{0} date: year = {1}, month = {2}, day = {3}".format(dateflag, yearstr, monthstr, daystr))
     try:
-        date_obj = datetime.datetime(int(yearstr), int(monthstr), int(daystr))
+        date_obj = date(int(yearstr), int(monthstr), int(daystr))
     except:
-        check_date(raw_input("Invalid date, please input again: "))
+        # date_obj = raw_input("Invalid date, please input again: ")
+        return get_date(raw_input("Invalid {} date, please input again: ".format(dateflag)), dateflag)
     else:
         return date_obj
 
-print(check_date(first_date) - check_date(second_date))
-duration_time = (check_date(first_date) - check_date(second_date)).days
-print("The duration time is: {}".format(abs(duration_time)))
+def get_date_diff(firstdate, seconddate):
+    """ Get duration between two dates. """
+    return abs((get_date(firstdate, "First") - get_date(seconddate, "Second")).days)
 
-#
+print('The date format is MM/DD/YY, for example: 11/29/2017')
+
+# Get user input and check date format
+first_date = check_date_format(raw_input('Please input first date: '))
+second_date = check_date_format(raw_input('Please input second date: '))
+
+print("The duration time is: {}".format(get_date_diff(first_date, second_date)))
+
+os.system('pause')
