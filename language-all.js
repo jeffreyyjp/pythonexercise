d804288b08313fe537bebf9ac7406578cdc8fff9
Jeffrey_Yang <gracyme123@hotmail.com>
2017-12-01 22:15:48 +0800

    fix bug

---
+++ language-all.js
@@ -0,0 +1,42 @@
+Config.language = {
+-------------------- BR0001 --------------------
+BR0001_eng : "Home page",
+BR0001_fre : "Page d'Accueil",
+BR0001_spa : "Página de inicio",
+-------------------- BR0002 --------------------
+BR0002_eng : "Action bar",
+BR0002_fre : "Barre d'Action",
+BR0002_spa : "Barra de acciones ",
+-------------------- BR0003 --------------------
+BR0003_eng : "Forward",
+BR0003_fre : "Avant",
+BR0003_spa : "Adelante ",
+-------------------- BR0004 --------------------
+BR0004_eng : "Add bookmark",
+BR0004_fre : "Ajouter un favoris",
+BR0004_spa : "Añadir marcador",
+-------------------- BR0005 --------------------
+BR0005_eng : "Delete bookmark",
+BR0005_fre : "Supprimer un signet",
+BR0005_spa : "Eliminar marcador ",
+-------------------- BR0006 --------------------
+BR0006_eng : "Add bookmark success",
+BR0006_fre : "Signet ajouté",
+BR0006_spa : "Marcador añadido con éxito",
+-------------------- BR0007 --------------------
+BR0007_eng : "Bookmarks have been added",
+BR0007_fre : "Les signets ont été ajoutés",
+BR0007_spa : "Se han añadido Marcadores",
+-------------------- BR0008 --------------------
+BR0008_eng : "Keypad",
+BR0008_fre : "Clavier",
+BR0008_spa : "Teclado",
+-------------------- BR0009 --------------------
+BR0009_eng : "Bookmarks",
+BR0009_fre : "Favoris",
+BR0009_spa : "Marcadores",
+-------------------- BR0010 --------------------
+BR0010_eng : "Exit",
+BR0010_fre : "Quitter",
+BR0010_spa : "Salir",
+}
--- languages.py
+++ languages.py
@@ -2,17 +2,29 @@
 
 import fileinput, glob
 
-lang_item = {}
+lang_dict = {}
+lang_files = glob.glob("language_*.js")
 
 def process(line):
+    """ Dealing each line and save it to a dict
+
+    """
     if line.find(':') != -1:
+        key, item = line.split(' : ')[0], line.split(" : ")[1]
+        file_name_flag = fileinput.filename().split("_")[1].replace(".js", "")
+        lang_dict[key + "_" + file_name_flag] = item
 
-        lang_item[line.split(" : ")[0] + "_" + fileinput.filename().split("_")[1]] = line.split(" : ")[1]
-
-for line in fileinput.input(glob.glob('language_*.js')):
+for line in fileinput.input(lang_files):
     process(line)
 
-with open("language_all.js", "wb+") as f:
+with open("language-all.js", "wb+") as f:
     f.write("Config.language = {\n")
-    for key in lang_item.keys():
-        f.write("{0} : {1}".format(key, lang_item(key)))
+    sorted_keys = lang_dict.keys()
+    sorted_keys.sort()
+    i = 0
+    for key in sorted_keys:
+        if i % len(lang_files) == 0:
+            f.write("-" * 20 + " " + key.split('_')[0] + " " + "-" * 20 + "\n")
+        f.write("{0} : {1}".format(key, lang_dict[key]))
+        i += 1
+    f.write("}")
